813ba2078eae8d6e52182b6c45d66d34e701d0e2
JoseManuel134975 <josemanuel3606@gmail.com>
2025-10-30 10:17:49 +0100

    vpc.tf change -> Problemas en grupo de seguridad

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -16,25 +16,26 @@ resource "aws_security_group" "allow_http_and_ssh" {
     protocol = "tcp"
     cidr_blocks =  ["0.0.0.0/0"]  
   
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
-    cidr_blocks =  ["0.0.0.0/0"]
+#     from_port = 8080
+#     to_port = 8080
+#     protocol = "tcp"
+#     cidr_blocks =  ["0.0.0.0/0"]
 
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks =  ["0.0.0.0/0"]
+#     from_port = 443
+#     to_port = 443
+#     protocol = "tcp"
+#     cidr_blocks =  ["0.0.0.0/0"]
 
-    from_port = 8443
-    to_port = 8443
-    protocol = "tcp"
-    cidr_blocks =  ["0.0.0.0/0"]
+#     from_port = 8443
+#     to_port = 8443
+#     protocol = "tcp"
+#     cidr_blocks =  ["0.0.0.0/0"]
 
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks =  ["0.0.0.0/0"]
+#     from_port = 22
+#     to_port = 22
+#     protocol = "tcp"
+#     cidr_blocks =  ["0.0.0.0/0"]
+#   
   }
 
   egress = {
