eecb617fe52a86163f584237e85509235ec64b40
JoseManuel134975 <josemanuel3606@gmail.com>
2025-10-30 09:08:55 +0100

    ec2-instance.tf finish

---
+++ terraform/ec2-instance.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "debian" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  availability_zone = "${var.aws_region}a"
+  subnet_id = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [ aws_security_group.allow_http_and_ssh.id ]
+  user_data = file("user_data.sh")
+
+  tags = {
+    Name = "ec2-debian"
+  }
+}
