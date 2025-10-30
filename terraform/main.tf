c87805bb7146e040cf374e0fcd8dd7af780c34a3
JoseManuel134975 <josemanuel3606@gmail.com>
2025-10-30 09:10:58 +0100

    main.tf finish

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 0.11.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
+  token = var.session_token
+}
+
+resource "aws_key_pair" "debian_pub" {
+  key_name = "debian_pub"
+  public_key = file("debian.pub")
+}
