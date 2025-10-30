2348fcd45b3151e97abd86fee95df9da5d2d538c
JoseManuel134975 <josemanuel3606@gmail.com>
2025-10-30 10:12:03 +0100

    cp

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -14,31 +14,23 @@ resource "aws_security_group" "allow_http_and_ssh" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks =  ["0.0.0.0/0"]
-  }
+    cidr_blocks =  ["0.0.0.0/0"]  
   
-  ingress = {
     from_port = 8080
     to_port = 8080
     protocol = "tcp"
     cidr_blocks =  ["0.0.0.0/0"]
-  }
 
-  ingress = {
     from_port = 443
     to_port = 443
     protocol = "tcp"
     cidr_blocks =  ["0.0.0.0/0"]
-  }
 
-  ingress = {
     from_port = 8443
     to_port = 8443
     protocol = "tcp"
     cidr_blocks =  ["0.0.0.0/0"]
-  }
 
-  ingress = {
     from_port = 22
     to_port = 22
     protocol = "tcp"
